bd06d8fb7d8768630d16a77a425e6bc46605e0d0
Shakaib1997 <shakaib97@gmail.com>
2016-04-02 15:51:15 -0400

    this is a change

--- Messenger/app/src/main/java/com/learning/shakaib/messenger/CreateMessageActivity.java
+++ Messenger/app/src/main/java/com/learning/shakaib/messenger/CreateMessageActivity.java
@@ -4,17 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CreateMessageActivity extends Activity {
+    @Bind(R.id.message) EditText messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+        ButterKnife.bind(this);
     }
 
     public void onSendMessage(View view){
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
         startActivity(intent);
     }
 }
--- Messenger/app/src/main/java/com/learning/shakaib/messenger/ReceiveMessageActivity.java
+++ Messenger/app/src/main/java/com/learning/shakaib/messenger/ReceiveMessageActivity.java
@@ -1,19 +1,25 @@
 package com.learning.shakaib.messenger;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.TextView;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class ReceiveMessageActivity extends Activity {
+    @Bind(R.id.message)
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
-        }
+
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+
     }
+}
 
 
