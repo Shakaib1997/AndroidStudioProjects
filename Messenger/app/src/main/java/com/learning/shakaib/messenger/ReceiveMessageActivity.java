0bc15b01db4e88145c04a59b883c02f2a727a1fe
Shakaib1997 <shakaib97@gmail.com>
2016-04-03 12:16:42 -0400

    Added butterknife to receivemessageactivity

--- Messenger/app/src/main/java/com/learning/shakaib/messenger/ReceiveMessageActivity.java
+++ Messenger/app/src/main/java/com/learning/shakaib/messenger/ReceiveMessageActivity.java
@@ -6,19 +6,20 @@ import android.content.Intent;
 import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ReceiveMessageActivity extends Activity {
-    @Bind(R.id.message)
+    @Bind(R.id.message) TextView messageView;
     public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
-
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         String messageText = intent.getStringExtra(EXTRA_MESSAGE);
-
+        messageView.setText(messageText);
     }
 }
 
